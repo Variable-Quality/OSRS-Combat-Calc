2f677660d4020bf18b2745c688af26d21e15f9ca
DJ Rice <deanrice44@gmail.com>
2023-03-23 11:29:33 -0400

    basic func

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 URLS = {"One Handed":"https://oldschool.runescape.wiki/w/Weapon_slot_table",
         "Two Handed":"https://oldschool.runescape.wiki/w/Two-handed_slot_table"}
@@ -9,7 +10,12 @@ soup = BeautifulSoup(data, 'html.parser')
 
 table = soup.find_all("tbody")[1]
 i = 0
-for row in table.find_all("tr"):
-    print(i)
-    i += 1
+for row in table.find_all("tr")[1:]:
+        row_info = row.find_all("td")
+        item_name = row_info[1].text
+        item_stats = row_info[3:]
+
+        print(f"\n{item_name}: \n")
+        print(item_stats)
+
 
